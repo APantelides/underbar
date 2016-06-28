3ee31d64091df1682114c570d893bd6b3c0ba40b
Alex Pantelides <alex.pantelides@gmail.com>
2016-06-28 12:43:34 -0700

    Implement _.reduce and make the tests pass and understand implementation of contains
    Reduce takes as arguments a collection, a function to be applied, and an optional start value. The iterator function takes in the start value and a collection element and returns a result which is the start value in the function which will be called on the next element. This reduces a collection into one result at the end. If a start value is not given it should use the first element as the start value. It should also call the function even if it will return undefined (not sure if I implemented that correctly).
    
    Contains checks a collection to see if it contains a target value. If it does it returns true. It uses reduce and an anonymous operator function to see if at least one of the elements contains the target value. The start value is false and as soon as the equality check returns true the function keeps returning true until it ends.

--- src/underbar.js
+++ src/underbar.js
@@ -160,6 +160,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(element) {
+
+      if(accumulator === undefined) {
+        accumulator = iterator(accumulator,element)
+
+        accumulator = element
+
+      } else {
+        accumulator = iterator(accumulator, element)
+      }
+    })
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
